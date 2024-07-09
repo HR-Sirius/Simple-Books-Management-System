724a963d248260a3c0dbe741f8e5d3050bb3a186
Sirius <142125821+THU-Sirius@users.noreply.github.com>
2024-07-09 21:56:33 +0800

    Update Users.h

--- 头文件/Users.h
+++ 头文件/Users.h
@@ -10,19 +10,41 @@
 #define _USERS
 
 #include "Management.h"
-#include "Lend.h"
+#include "Book.h"
+
+//为了方便排序时不丢失对书籍、用户编号的跟踪，建立结构体排序对
+struct sort_User_pair
+{
+
+};
 
 class Users :virtual public Management
 {
-	
+	//搜索、统计等功能需要用到Book类成员，申明为友元类
+	friend Book;
+
+	//统计最热门书籍,用于计数:pop_Book[50]
+	//统计最勤奋读者,用于计数:pop_User[50]
+	//最热门书籍:top_Book[5]，存放书籍编号
+	//最勤奋读者:top_Users[3]，存放用户编号
+	static int pop_Book[50];
+	static int pop_User[50];
+	static int top_Books[5];
+	static int top_Users[3];
+
 public:
-	//静态对象，用于计数
+	//静态对象，自动生成用户编号
 	static int currentUsers;
 	
 	//输出用户借阅历史:Showhistory()
 	void Showhistory();
 	//接口函数，返回用户姓名
 	char* GetName();
+	//输出统计结果:Show_top_Books(),Show_top_Users()
+	void Show_top_Books();
+	void Show_top_Users();
+	//搜索方法(最重要函数)，由于搜索结果是书名若干，因此计划将搜索结果保存在临时创建的数组中，逐一打印
+	void Search() const;
 
 	//无参构造函数User(),实现用户自动编号功能
 	//析构函数~Users(),提示作用(可之后改为虚析构函数)
@@ -32,8 +54,14 @@ public:
 
 	//通过读文件打印用户信息:Getinfo()
 	//通过写文件更改用户信息:Resetinfo()
+	//借阅操作,统计借阅数据:Borrow(int)
+	//还书:Return(int)
+	//更改用户借阅历史:Resetinfo()
 	virtual void Getinfo() const;
 	virtual void Resetinfo();
+	void Borrow();
+	void Return();
+	virtual void Resethistory(int);
 
 	//流输入运算符重载
 	friend std::istream& operator>>(istream&, Users&);
@@ -52,6 +80,7 @@ protected:
 	int Borrowing[3] = { 0 };//之后可添加身份属性，不同身份有着不同的借阅上限
 };
 
+//静态成员初始化
 int Users::currentUsers = 0;
 
 //通过对象数组创建Users类全局对象
