b7d9a7ce3fe4bab3ff352ea619f346277c26f094
Sirius <142125821+THU-Sirius@users.noreply.github.com>
2024-07-22 17:15:00 +0800

    Update Management.h

--- 头文件/Management.h
+++ 头文件/Management.h
@@ -30,7 +30,6 @@ struct Num_Pair
 class Management
 {
 public:
-	virtual void Setinfo(int) = 0;
 	virtual void Getinfo(int) = 0;
 	virtual void Resetinfo(int) = 0;
 	//排序:Bubble_sort()
@@ -52,12 +51,12 @@ void Management::Bubble_sort(int pop_[], int top_[], int top_length)
 	//暂定数组长度均为10，之后可改为动态创建数组
 	//将pop_[]的值从大到小排列，不改变原数组的值
 
-	int* parray[10];
-	int subnote[10] = {0,1,2,3,4,5,6,7,8,9};	//记录每一次交换的下标
+	int* parray[20];
+	int subnote[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};	//记录每一次交换的下标
 	int i, j;
 
 	//parray[i]->pop_[i]
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 20; i++)
 	{
 		parray[i] = &pop_[i];
 	}
@@ -65,7 +64,7 @@ void Management::Bubble_sort(int pop_[], int top_[], int top_length)
 	//从大到小冒泡排序
 	int* temp = NULL;
 	int Temp = 0;
-	for (j = 9; j > 0; j--)
+	for (j = 19; j > 0; j--)
 		for (i = 0; i < j; i++)
 		{
 			if (*parray[i] < *parray[i + 1])
